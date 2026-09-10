cfa84c6d44e727b9380b9c6ee65e09ac90990f3a
Fikri R. Nurhidayat <fikrirnurhidayat@gmail.com>
2026-09-10 15:55:29 +0700

    feat(entry): add context aware redirection on entry tap

--- lib/modules/entries/presentation/views/entry_list_view.dart
+++ lib/modules/entries/presentation/views/entry_list_view.dart
@@ -1,6 +1,10 @@
+import 'package:bandha/core/presentation/models/draft.dart';
 import 'package:bandha/core/presentation/views/async_list_view.dart';
 import 'package:bandha/modules/entries/domain/entities/entry.dart';
 import 'package:bandha/modules/entries/presentation/widgets/entry_tile.dart';
+import 'package:bandha/modules/funds/domain/entities/fund.dart';
+import 'package:bandha/modules/journals/domain/entities/journal.dart';
+import 'package:bandha/modules/transfers/domain/entities/transfer.dart';
 import 'package:flutter/widgets.dart';
 
 class EntryListView extends StatelessWidget {
@@ -12,6 +16,40 @@ class EntryListView extends StatelessWidget {
       context,
       name: 'Entries',
       tileBuilder: EntryTile.builder,
+      onTileTap: (context, item) async {
+        if (item.entity.controller != null) {
+          final controllerType = item.entity.controller!.type;
+          final entityId = item.entity.controller!.id;
+
+          switch (controllerType) {
+            case "Fund":
+              await Navigator.pushNamed<Draft<Fund>>(
+                context,
+                "/funds/$entityId/entries",
+              );
+              return;
+            case "Journal":
+              await Navigator.pushNamed<Draft<Journal>>(
+                context,
+                "/journals/$entityId/entries",
+              );
+              return;
+            case "Transfer":
+              await Navigator.pushNamed<Draft<Transfer>>(
+                context,
+                "/transfers/$entityId/entries",
+              );
+              return;
+            default:
+              return;
+          }
+        }
+
+        await Navigator.pushNamed<Draft<Entry>>(
+          context,
+          "/entries/${item.entity.id}/detail",
+        );
+      },
     );
   }
 }
